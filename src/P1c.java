b6e6c63b5af6b5ddaccbfc3a4959c97f7f9b7515
Glenn McGookin <glennyboi1@me.com>
2018-09-27 19:40:41 +0100

    P1c

---
+++ src/P1c.java
@@ -0,0 +1,18 @@
+public class P1c {
+    public static void main(String[] args) {
+        System.out.println("Hello World!\tI am alive.");
+
+        System.out.println("\nHello World!\tI am alive.\nI can write two lines!\n");
+
+        System.out.println("Happy Christmas\n");
+        System.out.println("     *     ");
+        System.out.println("    * *    ");
+        System.out.println("   * * *   ");
+        System.out.println("  * * * *  ");
+        System.out.println(" * * * * * ");
+        System.out.println("* * * * * *");
+
+        System.out.println("\nFirst Witch\t\tWhen shall we three meet again?\n\t\t\t\tIn thunder, lightning, or in rain?");
+
+    }
+}
