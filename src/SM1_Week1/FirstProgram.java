61cccd44ab0be37e02f5b0aab511a62e348e2faf
Glenn McGookin <glennyboi1@me.com>
2018-09-30 12:49:22 +0100

    Updates to the project

--- src/SM1_Week1/FirstProgram.java
+++ src/SM1_Week1/FirstProgram.java
@@ -1,10 +1,14 @@
 package SM1_Week1;
 
 /**
- * @author glennmcgookin
- * @version %I%, %G%
- * @since 1.0
+ * @file        FirstProgram
+ * @version     %I%, %G%
+ * @since       1.0
+ * @author      Glenn McGookin
+ * Created on:  30/09/2018
+ * Description: A program designed to compiile and print "Hello World" to the console
  */
+
 
 public class FirstProgram {
     public static void main(String[] args) {
