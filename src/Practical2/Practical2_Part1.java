70fe1445ea1fa9c9e19e45b4a2fa71a1c1b126e8
Glenn McGookin <glennyboi1@me.com>
2018-10-02 19:32:49 +0100

    Practical 2 - Part 1 commit

--- src/Practical2/Practical2_Part1.java
+++ src/Practical2/Practical2_Part1.java
@@ -21,7 +21,7 @@ public class Practical2_Part1 {
         System.out.println("\nYour name is: " + yourName);
         System.out.println("The number of characters in the string: " + yourName.length());
         System.out.println("The first character in the string is " + yourName.charAt(0) + " \nThe second character is: " + yourName.charAt(0+1));
-        System.out.println("The string in uppercase:" + yourName.toUpperCase());
+        System.out.println("The string in uppercase: " + yourName.toUpperCase());
         System.out.println("All instances of the letter 'n' replaced with '%': " + yourName.replace('n','%'));
         System.out.println("The position of the first 'n' is: " + yourName.indexOf('n') + "\nThe last 'n' is located at: " + yourName.lastIndexOf('n'));
     }
