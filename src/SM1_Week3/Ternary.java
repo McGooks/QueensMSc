303199be9db739255de9288748cdf12f88e930d1
Glenn McGookin <glennyboi1@me.com>
2018-10-08 21:17:53 +0100

    Week 3 commit

--- src/SM1_Week3/Ternary.java
+++ src/SM1_Week3/Ternary.java
@@ -3,11 +3,11 @@ package SM1_Week3;
 import java.util.Scanner;
 
 /**
- * @file            Selection.java
+ * @file            Tenary.java
  * @version         1.0
  * @author          Glenn McGookin
  * Created on:      08/10/2018
- * Description:     A program designed to ask the user to insert their age, and the program will use an if else
+ * Description:     A program designed to ask the user to insert their age, and the program will use a Ternary
  *                  statement to print an output to the console based on the integer value inserted
  */
 
