a5db1284fb09c5f426860f621cc152f0220a8425
Glenn McGookin <glennyboi1@me.com>
2018-10-17 20:29:41 +0100

    Week 4 commit

---
+++ src/SM1_Week4/Q7.java
@@ -0,0 +1,11 @@
+package SM1_Week4;
+
+public class Q7 {
+    public static void main(String[] args) {
+        int check = 5;
+
+        while( check == 5)
+            System.out.println("Executing loop");
+        check++;
+    }
+}
