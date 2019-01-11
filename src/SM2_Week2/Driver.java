572b0689e81260c1f74c244374ab904129a09163
Glenn McGookin <glenn.mcgookin@icloud.com>
2019-01-11 12:56:53 +0000

    Commit

---
+++ src/SM2_Week2/Driver.java
@@ -0,0 +1,11 @@
+package SM2_Week2;
+
+public class Driver {
+    public static void main(String[] args) {
+        SimpleCalc adding = new SimpleCalc();
+        adding.add(15,3);
+        adding.subtract(9,2);
+        adding.multiply(3,2);
+        adding.divide(9,3);
+    }
+}
---
+++ src/SM2_Week2/SimpleCalc.java
@@ -0,0 +1,28 @@
+package SM2_Week2;
+
+public class SimpleCalc {
+
+    public void add(int num1, int num2){
+        int total;
+        total = num1 + num2;
+        System.out.println(num1 + " + " + num2 + " = " + total);
+    }
+
+    public void subtract(int num1, int num2){
+        int total;
+        total = num1 - num2;
+        System.out.println(num1 + " - " + num2 + " = " + total);
+    }
+
+    public void multiply(int num1, int num2){
+        int total;
+        total = num1 * num2;
+        System.out.println(num1 + " x " + num2 + " = " + total);
+    }
+
+    public void divide(int num1, int num2){
+        int total;
+        total = num1 * num2;
+        System.out.println(num1 + " / " + num2 + " = " + total);
+    }
+}
